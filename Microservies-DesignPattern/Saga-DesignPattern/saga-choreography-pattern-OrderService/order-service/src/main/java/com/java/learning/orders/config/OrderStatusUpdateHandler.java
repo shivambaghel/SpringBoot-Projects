801a9dbb19773159c534bdf22929aae88d5d453c
baghel <singhsbaghel96@gmail.com>
2026-09-22 07:04:53 +0530

    Feat: OrderService - consume event from payment-event Topic and based on status  update the result

---
+++ Microservies-DesignPattern/Saga-DesignPattern/saga-choreography-pattern-OrderService/order-service/src/main/java/com/java/learning/orders/config/OrderStatusUpdateHandler.java
@@ -0,0 +1,49 @@
+package com.java.learning.orders.config;
+
+import com.java.learning.common.dto.OrderRequestDto;
+import com.java.learning.common.event.OrderStatus;
+import com.java.learning.common.event.PaymentStatus;
+import com.java.learning.orders.entity.PurchaseOrder;
+import com.java.learning.orders.repository.OrderRepository;
+import com.java.learning.orders.service.OrderStatusPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class OrderStatusUpdateHandler {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Autowired
+    private OrderStatusPublisher publisher;
+
+
+    // Check if payment status is completed , update order status as completed or cancelled
+    @Transactional
+    public void updateOrder(int id, Consumer<PurchaseOrder> consumer) {
+        repository.findById(id).ifPresent(consumer.andThen(this::updateOrder));
+    }
+
+    private void updateOrder(PurchaseOrder purchaseOrder) {
+        boolean isPaymentComplete = PaymentStatus.PAYMENT_COMPLETED.equals(purchaseOrder.getPaymentStatus());
+        OrderStatus orderStatus = isPaymentComplete ? OrderStatus.ORDER_COMPLETED : OrderStatus.ORDER_CANCELLED;
+        purchaseOrder.setOrderStatus(orderStatus);
+        if (!isPaymentComplete) {
+            publisher.publishOrderEvent(convertEntityToDto(purchaseOrder), orderStatus);
+        }
+    }
+
+    public OrderRequestDto convertEntityToDto(PurchaseOrder purchaseOrder) {
+        OrderRequestDto orderRequestDto = new OrderRequestDto();
+        orderRequestDto.setOrderId(purchaseOrder.getId());
+        orderRequestDto.setUserId(purchaseOrder.getUserId());
+        orderRequestDto.setAmount(purchaseOrder.getPrice());
+        orderRequestDto.setProductId(purchaseOrder.getProductId());
+        return orderRequestDto;
+    }
+}
+
